365d31641444efe5ead4be0840c1505a1532c74c
Klemensas Kaubrys <kaubrys.klemensas@outlook.com>
2024-10-05 22:54:45 +0300

    Added cool colored letter

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 const express = require('express');
-const path = require('path'); 
+const path = require('path');
+const https = require('https');
+const fs = require('fs'); 
+const app = express(); 
 const favicon = require('serve-favicon');
 
-const app = express(); 
-const PORT = 8080;
+// Load SSL certificate and private key
+const options = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert'),
+  };
 
+const PORT = 443;
 
+// Serving the favicon
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serving the html files
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.use((req, res) => {
     res.status(404).send('404 Not Found');
 });
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
+https.createServer(options, app).listen(PORT, () => {
+    console.log(`Server is running on https://localhost:${PORT}`);
+  });
