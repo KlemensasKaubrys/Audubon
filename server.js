7d9be45b90e4e0296ef6429abf205a1cfeca17b4
Klemensas Kaubrys <kaubrys.klemensas@outlook.com>
2024-10-05 20:19:35 +0300

    Added favicon

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path'); 
+const favicon = require('serve-favicon');
 
 const app = express(); 
 const PORT = 8080;
 
-app.use(express.static(__dirname));
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
